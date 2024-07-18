a0add0c32b88559a844a9af51674fe71021b575e
kmv <117433630+rkrch@users.noreply.github.com>
2024-07-18 12:30:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://v6.exchangerate-api.com/v6/YOUR_API_KEY/latest/USD")
+fetch("https://v6.exchangerate-api.com/v6/466f834ac2173f991f9c364d/latest/USD")
 .then(response => response.json())
 .then(data => {
     const currencyDiv = document.getElementById('currency');
